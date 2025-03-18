0143756d14a178e022448dcaa5cdfd3ba2bb3940
Wen Xuan <138146834+wenxuangithub@users.noreply.github.com>
2025-03-18 19:28:29 +0800

    initial commit

---
+++ MudBlazorWebAppVS/Program.cs
@@ -0,0 +1,38 @@
+using MudBlazor.Services;
+using MudBlazorWebAppVS.Client.Pages;
+using MudBlazorWebAppVS.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(MudBlazorWebAppVS.Client._Imports).Assembly);
+
+app.Run();
